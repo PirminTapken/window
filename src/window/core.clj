abd0a9c9c4dec92052f105bb94693c28ee767f3e
Pirmin Fix <pirmin@42degreesoffreedom.com>
2011-03-07 21:45:30 +0100

    working actionlistener

--- src/window/core.clj
+++ src/window/core.clj
@@ -1,1 +1,32 @@
-(ns window.core)
+(ns window.core
+  (:gen-class)
+  (:import (java.awt Frame Window))
+  (:import (java.awt.event WindowListener)))
+
+(defn main-window 
+  "properties are map with following keys:
+  :size-x
+  :size-y
+  :title
+  :visible"
+  [properties]
+  (doto (Frame.)
+    (.setTitle (if-let [s (:title properties)] s ""))
+    (.setSize
+      (if-let [x (:size-x properties)] x 0)
+      (if-let [y (:size-y properties)] y 0))
+    (.addWindowListener (if-let [l (:listener properties)] l nil))
+    (.setVisible (if-let [b (:visible properties)] b false))))
+
+(def window-listener
+  (reify
+    WindowListener
+      (windowClosing [this event] (.dispose (.getWindow event)))))
+
+(defn -main []
+  (main-window
+    {:size-x 640
+     :size-y 480
+     :title "stuff"
+     :listener window-listener
+     :visible true}))
