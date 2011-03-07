2bb90dc6f249bd6ac234a09a149ddc0f92f8faa0
Pirmin Fix <pirmin@42degreesoffreedom.com>
2011-03-07 21:47:15 +0100

    lein and git stuff

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject window "1.0.0-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :main window.core)
