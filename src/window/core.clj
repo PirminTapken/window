8ccb075121b08c7b4c9a49a6dfc7e7bd33ee815a
Pirmin Fix <pirmin.fix@desy.de>
2011-03-08 09:58:29 +0100

    fixed interface 'bug'

--- src/window/core.clj
+++ src/window/core.clj
@@ -1,7 +1,7 @@
 (ns window.core
   (:gen-class)
   (:import (java.awt Frame Window))
-  (:import (java.awt.event WindowListener)))
+  (:import (java.awt.event WindowAdapter)))
 
 (defn main-window 
   "properties are map with following keys:
@@ -18,15 +18,14 @@
     (.addWindowListener (if-let [l (:listener properties)] l nil))
     (.setVisible (if-let [b (:visible properties)] b false))))
 
-(def window-listener
-  (reify
-    WindowListener
-      (windowClosing [this event] (.dispose (.getWindow event)))))
+(def window-adapter
+  (proxy [WindowAdapter] []
+      (windowClosing [event] (.dispose (.getWindow event)))))
 
 (defn -main []
   (main-window
     {:size-x 640
      :size-y 480
      :title "stuff"
-     :listener window-listener
+     :listener window-adapter
      :visible true}))
